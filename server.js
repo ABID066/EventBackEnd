85651ab982858c5e25614109e864396bb12ccf7b
ABID066 <mdabid1152@gmail.com>
2025-04-26 21:49:25 +0600

    server.js file update

--- server.js
+++ server.js
@@ -39,5 +39,10 @@ app.use(express.json());  // To parse JSON request bodies
 app.use('/api/auth', authRoutes);
 app.use('/api/events', protect, eventRoutes); // Protect routes
 
+
+app.use("*", (req, res) => {
+    res.status(404).json({ msg: "Wrong URL" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
