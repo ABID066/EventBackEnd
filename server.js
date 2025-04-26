73512df58661bb4a496b72c7f5be7e6faefb70fd
ABID066 <mdabid1152@gmail.com>
2025-04-26 22:10:49 +0600

    server.js file update

--- server.js
+++ server.js
@@ -40,9 +40,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/events', protect, eventRoutes); // Protect routes
 
 
-app.use("*", (req, res) => {
-    res.status(404).json({ msg: "Wrong URL" });
-});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
